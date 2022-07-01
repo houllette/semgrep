0f472746b2c4c609dec7942e62cad7abfe9dbf6d
Trevor Fenn <sgtpepper43@gmail.com>
2022-06-30 18:05:59 -0600

    Allow multiple lines in a block!

--- semgrep-core/tests/elixir/parsing/hello_world.ex
+++ semgrep-core/tests/elixir/parsing/hello_world.ex
@@ -2,4 +2,9 @@ defmodule Hello do
   def hello do
     IO.puts("Hello world!!")
   end
+
+  def hello(world) do
+    str = "Hello" <> world
+    IO.puts(str)
+  end
 end
--- semgrep-core/tests/elixir/parsing/hello_world.rb
+++ semgrep-core/tests/elixir/parsing/hello_world.rb
@@ -2,4 +2,9 @@ module Hello
   def hello
     IO.puts("Hello world!!")
   end
+
+  def hello(world)
+    str = "Hello" + world
+    IO.puts(str)
+  end
 end
